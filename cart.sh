4fa9cb069ec9ee1f30c1527b9e1a855f79af7b2b
jhonnMike <jhonnmike@gmail.com>
2022-08-14 13:30:29 +0530

    added msgs

--- cart.sh
+++ cart.sh
@@ -22,6 +22,7 @@
  echo "configuring cart systemD service"
  mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>> /tmp/cart.log
  systemctl daemon-reload &>> /tmp/cart.log
+ 
  echo "starting cart services"
  systemctl start cart &>> /tmp/cart.log
  systemctl enable cart &>> /tmp/cart.log
